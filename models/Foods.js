e23a036f34a6cee8b6755b93e801fd527f928fc4
Miky.Gabry <isgromichael@gmail.com>
2023-05-17 20:23:26 -0400

    Create models Foods.js and index.js. Linked with mongoDB

---
+++ models/Foods.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const foodsSchema = new mongoose.Schema(
+    {
+        dishName: {
+            type: String,
+            required: [true, "It must have a name!"],
+            unique: true
+        },
+        ingredients: {
+            type: String,
+            required: [true, "Ingredients go here"]
+        },
+        cuisine: {
+            type: String,
+        },
+        recipe: {
+            type: String,
+            required: [true, "We do need the prep instructions"]
+        },
+        dishImage: {
+            type: String,
+            required: [true]
+        },
+        dishVideo: {
+            type: String
+        }
+    },
+    {
+        timeseries: true
+    }
+);
+
+const Foods = mongoose.model('food', foodsSchema);
+
+module.exports = Foods;
